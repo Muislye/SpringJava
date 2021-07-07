7f12e13d17df8b1be8748a15ae21154e27344e3e
Muislye <64579338+Muislye@users.noreply.github.com>
2021-07-07 10:01:52 -0500

    first commit

---
+++ platzi-matket/src/main/java/com/platzi/matket/PlatziMatketApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.matket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMatketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMatketApplication.class, args);
+	}
+
+}
---
+++ platzi-matket/src/test/java/com/platzi/matket/PlatziMatketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.matket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziMatketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
